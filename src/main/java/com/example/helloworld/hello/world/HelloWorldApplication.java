86bb15d5899e882184787a19caeaa177a3999e61
rafaelspa <rsdepaula67@gmail.com>
2023-07-06 14:08:19 -0300

    start

---
+++ src/main/java/com/example/helloworld/hello/world/HelloWorldApplication.java
@@ -0,0 +1,16 @@
+package com.example.helloworld.hello.world;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldApplication.class, args);
+		System.out.println("Hello world");
+	}
+
+
+
+}
