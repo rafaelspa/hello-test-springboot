9ffba9bd5aaa785e7cba99e5ae412d27881ce2de
rafaelspa <rsdepaula67@gmail.com>
2023-11-28 18:05:49 -0300

    add tests

--- src/test/java/com/example/helloworld/hello/world/HelloWorldApplicationTests.java
+++ src/test/java/com/example/helloworld/hello/world/HelloWorldApplicationTests.java
@@ -1,13 +1,25 @@
 package com.example.helloworld.hello.world;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
 
 @SpringBootTest
 class HelloWorldApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void twoIsEven() {
+		Assertions.assertEquals(0, 2%2);
+	}
+
+	@Test
+	void threeIsOdd() {
+		Assertions.assertEquals(0, 3%2);
 	}
 
 }
